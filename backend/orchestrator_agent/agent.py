ad58739fa9e3c6926b0006e9d342f3edd4fbec0c
jobanxd <sethjovan.pacturanan@gmail.com>
2025-09-06 01:10:47 +0800

    added sub agent regarding trivia agent that give random trivias

--- backend/orchestrator_agent/agent.py
+++ backend/orchestrator_agent/agent.py
@@ -1,6 +1,13 @@
 from google.adk.agents import Agent, LlmAgent
 from .agents.agentprofiles import AgentProfile
 
+trivia_agent_profile = AgentProfile(agentprofilename="trivia_agent")
+trivia_agent = LlmAgent(
+    name=trivia_agent_profile.name,
+    model=trivia_agent_profile.model_id,
+    description=trivia_agent_profile.description,
+    instruction=trivia_agent_profile.instruction,
+)
 
 root_agent_profile = AgentProfile(agentprofilename="root_agent")
 root_agent = Agent(
@@ -8,4 +15,7 @@ root_agent = Agent(
     model=root_agent_profile.model_id,
     description=root_agent_profile.description,
     instruction=root_agent_profile.instruction,
+    sub_agents=[
+        trivia_agent,
+    ],
 )
